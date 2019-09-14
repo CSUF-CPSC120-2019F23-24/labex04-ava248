320114caf0d1fa269a18e512e584eb6bad80094b
avahess <avahess@cs104u-3f.acad.fullerton.edu>
2019-09-14 14:30:07 -0700

    predicted sales

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,19 @@
 // Sales prediction
+
+#include <iostream>
+
+int main()
+{
+  //Show the total sales
+  const long TOTAL_SALES = 2103419277;
+  const long long TODAYS_SALES = TOTAL_SALES * 1.18;
+
+
+  std::cout << "Last year's sales were $" << TOTAL_SALES << std::endl;
+  std::cout << "This year's sales prediction: $" << TODAYS_SALES << std::endl;
+
+
+
+
+  return 0;
+}
